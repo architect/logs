281264bb12bf9163094c65f50dc0b188e8dd0d9a
Brian LeRoux <b@brian.io>
2019-08-12 12:26:22 -0700

    adds cli back into module

---
+++ cli.js
@@ -0,0 +1,22 @@
+let logs = require('.')
+
+let known = 'logs -v --verbose verbose -n --nuke nuke -p --production production prod'.split(' ')
+let verboseFlags = '-v --verbose verbose'.split(' ')
+let nukeFlags = '-n --nuke nuke'.split(' ')
+let productionFlags = '-p --production production prod'.split(' ')
+
+/**
+ * arc logs src/http/get-index ................... gets staging logs
+ * arc logs production src/http/get-index ........ gets production logs
+ * arc logs nuke src/http/get-index .............. clear staging logs
+ * arc logs nuke production src/http/get-index ... clear staging logs
+ */
+module.exports = function cli(opts) {
+
+  let pathToCode = opts.find(opt=> !known.includes(opt))
+  let verbose = opts.some(opt=> verboseFlags.includes(opt))
+  let nuke = opts.some(opt=> nukeFlags.includes(opt))
+  let production = opts.some(opt=> productionFlags.includes(opt))
+
+  return logs({ pathToCode, verbose, nuke, production })
+}
