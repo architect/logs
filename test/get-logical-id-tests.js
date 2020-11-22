ee1dd2e90716e9fbbc9fcf33f1cc4e96e328ff22
Ryan Block <rblock@gmail.com>
2020-11-22 14:40:59 -0800

    Windows

--- test/get-logical-id-tests.js
+++ test/get-logical-id-tests.js
@@ -1,5 +1,6 @@
 let test = require('tape')
 let _inventory = require('@architect/inventory')
+let { sep } = require('path')
 let getLogicalID = require('../src/get-logical-id')
 let inventory
 
@@ -18,22 +19,22 @@ test('Set up env', t => {
 
 test('should return logical id for path', t => {
   t.plan(1)
-  t.equal(getLogicalID(inventory, './src/http/get-index'), 'GetIndexHTTPLambda')
+  t.equal(getLogicalID(inventory, `.${sep}src${sep}http${sep}get-index`), 'GetIndexHTTPLambda')
 })
 
 test('should return logical id for websocket path', t => {
   t.plan(1)
-  t.equal(getLogicalID(inventory, './src/ws/default'), 'DefaultWSLambda')
+  t.equal(getLogicalID(inventory, `.${sep}src${sep}ws${sep}default`), 'DefaultWSLambda')
 })
 
 test('should replace references to 000 in path', t => {
   t.plan(1)
-  t.equal(getLogicalID(inventory, './src/http/get-api-000version'), 'GetApiVersionHTTPLambda')
+  t.equal(getLogicalID(inventory, `.${sep}src${sep}http${sep}get-api-000version`), 'GetApiVersionHTTPLambda')
 })
 
 test('should blow up on unknown path', t => {
   t.plan(1)
   t.throws(() => {
-    getLogicalID(inventory, './src/http/idk')
+    getLogicalID(inventory, `.${sep}src${sep}http${sep}idk`)
   }, 'Threw on unknown path')
 })
