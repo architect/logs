57cea3457bc08f61bb405dde1fd7754be14b84c9
Ryan Block <rblock@gmail.com>
2022-01-25 20:22:45 -0800

    Add support for Arc 10 custom lambdas - Add `npx arc-logs` direct invocation - Breaking change: bare CLI arguments (e.g. `logs destroy`) as aliases to flags are no longer used - Tidy up some old code paths - Update deps and changelog

--- src/validate.js
+++ src/validate.js
@@ -1,16 +1,9 @@
 let { updater } = require('@architect/utils')
 let update = updater('Logs')
 
-module.exports = function validate (/* opts*/) {
-  try {
-    if (process.env.ARC_AWS_CREDS === 'missing')
-      throw Error('missing or invalid AWS credentials or credentials file')
-
-    if (!process.env.AWS_REGION)
-      throw Error('@aws region / AWS_REGION must be configured')
-  }
-  catch (e) {
-    update.error(`Failed to access logs, ${e.message}`)
+module.exports = function validate () {
+  if (process.env.ARC_AWS_CREDS === 'missing') {
+    update.error('Failed to access logs: missing or invalid AWS credentials or credentials file')
     process.exit(1)
   }
 }
